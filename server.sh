7126eae447b759ebea685e48db1d10354958f3c2
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-08-08 13:53:57 +0900

    servermanager　シェルスクリプトなんもわからんわ

---
+++ server.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+servername="bunkyo_2024proc5_server"
+apacheConfigFilePath="httpd.conf"
+mariadbConfigFilePath="my.cnf"
+fastapifilePath="app.py"
+mode=$1
+
+function NewSession() {
+    if tmux list-sessions | grep -q "^$1*"; then
+        echo "$servername session is already on"
+    else
+        tmux new -d -s $1 -n dummy
+    fi
+}
+function NewWindow() {
+    if tmux list-windows -F '#S:#W' | grep -q "^$1\:$2"; then
+        echo "$2 window is already on"
+    else
+        tmux new-window -a -t $1 -n $2
+    fi
+}
+function KillWindow() {
+    if tmux list-windows -F '#S:#W' | grep -q "^$1:$2\$"; then
+        echo "killing $2 window"
+        tmux kill-window -t $1:$2
+    else
+        echo "$2 wasn't found"
+    fi
+}
+function SendKey() {
+    tmux send-keys -t "$1":"$2" "$3" C-m
+}
+
+modes=("start" "stop" "test")
+if ! printf '%s\n' "${modes[@]}" | grep -qx "$1"; then
+    echo "select function: { start | stop }"
+    exit 1
+fi
+
+if [ "$mode" == "start" ]; then
+    NewSession $servername
+    NewWindow $servername httpd
+    SendKey $servername httpd "httpd -d ./ -f $apacheConfigFilePath"
+    NewWindow $servername mariadb
+    SendKey $servername mariadb "mysqld --defaults-file=$mariadbConfigFilePath"
+    NewWindow $servername fastapi
+    SendKey $servername fastapi "python3.11 $fastapifilePath"
+    KillWindow $servername dummy
+elif [ "$mode" == "stop" ]; then
+    echo "not implemented yet"
+    #TODO: implement stop function
+elif [ "$mode" == "test" ]; then
+    SendKey $servername "httpd" "ls"
+fi
+exit 0
