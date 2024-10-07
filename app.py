1334ac92a4a3b336d3803aaafaf6bfcbe370a2a1
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-10-07 12:59:10 +0900

    テスト

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/v1/")
+@app.get("/")
 async def root():
-    return {"message": "hello world!"}
+    return {"message": "hello world! access v1 directory"}
+
+@app.get("/v1/")
+async def v1root():
+    return {"message": "incorrect usage"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3000, log_level="debug")
-
