6587073945cb666d8dd200e79a39bfe9be3fe044
[sakieda] <[c3p31044@bunkyo.ac.jp]>
2024-11-13 21:07:28 +0900

    データを入れた

--- probc2024-sd5.sql
+++ probc2024-sd5.sql
@@ -45,6 +45,28 @@ CREATE TABLE `products` (
   `kg_per_1a` int(50) NOT NULL,
   `yen_per_1a` int(50) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `products`
+--
+
+INSERT INTO `products` (`ID`, `name`, `summary`, `desc`, `product_categories_ID`, `kg_per_1a`, `yen_per_1a`) VALUES
+(1, 'あんぽ柿', '品種は平核無柿、蜂屋柿など', NULL, 1, 78, 240),
+(2, 'いちご', '品種はとちおとめ、さちのか、など', NULL, 2, 218, 1190),
+(3, 'いんげん', '品種はいちず、鴨川グリーンなど', NULL, 2, 72, 8678),
+(4, 'きゅうり', '品種はアンコール10、パイロット2号、南極1号など', NULL, 2, 580, 233),
+(5, 'さくらんぼ', '品種は佐藤錦など', NULL, 1, 0, 0),
+(6, 'さやえんどう', '品種は姫みどり、ゆうさやなど', NULL, 2, 43, 1157),
+(7, 'しいたけ', NULL, NULL, 3, 0, 928),
+(8, '春菊', NULL, NULL, 2, 119, 516),
+(9, '西洋なし', '品種はラ・フランス、ル・レクチェ、ゼネラル・レクラークなど', NULL, 1, 157, 373),
+(10, 'ニラ', '品種はワンダーグリーン、パワフルグリーンベルトなど', NULL, 2, 168, 546),
+(11, '花わさび', NULL, NULL, 2, 0, 0),
+(12, 'ピーマン', NULL, NULL, 2, 346, 346),
+(13, 'ぶどう', '品種は巨峰、高尾など', NULL, 1, 94, 1062),
+(14, '桃', '品種はあかつき、川中島白桃、ゆうぞらなど', NULL, 1, 158, 695),
+(15, 'りんご', '品種は王林、ふじなど', NULL, 1, 159, 287);
+
 -- --------------------------------------------------------
 --
 -- テーブルの構造 `product_categories`
@@ -55,6 +77,15 @@ CREATE TABLE `product_categories` (
   `name` text NOT NULL,
   `summary` text DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+--
+-- テーブルのデータのダンプ `product_categories`
+--
+
+INSERT INTO `product_categories` (`ID`, `name`, `summary`) VALUES
+(1, '果樹類', NULL),
+(2, '野菜類', NULL),
+(3, 'きのこ類', NULL);
 -- --------------------------------------------------------
 --
 -- テーブルの構造 `contacts`
