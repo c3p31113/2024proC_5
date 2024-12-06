8ab3cede23e27e6094523ed29d4bdcbdaedd98ff
LAVIE-KP\k-sei <c3p31096@bunkyo.ac.jp>
2024-12-06 21:22:41 +0900

    makedoc

--- api/make_doc.py
+++ api/make_doc.py
@@ -121,6 +121,9 @@ def main(form: Form, doc: DocumentObject = DOC):
         # 年間農業粗収益を入力
         table_replace("@income", str(income))
 
+        # 常時雇いの見通し人数（年間）
+        table_replace("@人数","")
+
     # 農業粗収益
     table_replace("農業粗収益の計算", str())
     makedirs("./tmp/", exist_ok=True)
